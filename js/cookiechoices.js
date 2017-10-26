a7f50d4a445d27fcc1837d0e6a68d4cd4b88d4a0
Emmanuel Plougoulm <contact@emmanuelplougoulm.fr>
2017-10-26 14:23:42 +0200

    add navbar responsive and burger menu + start responsive demarche qualité

--- js/cookiechoices.js
+++ js/cookiechoices.js
@@ -32,7 +32,7 @@
     var dismissLinkId = 'cookieChoiceDismiss';
 
     function _createHeaderElement(cookieText, dismissText, linkText, linkHref) {
-      var butterBarStyles = 'position:fixed;width:100%;background-color:#F69229;color:#000;font-size:14px;' +
+      var butterBarStyles = 'position:fixed;width:100vw;background-color:#F69229;color:#000;font-size:14px;' +
           'margin:0; left:0; top:0; padding:4px;z-index:1000;text-align:center;';
 
       var cookieConsentElement = document.createElement('div');
