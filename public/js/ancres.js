ff652fa945e1a713997e14e964135f40145afc93
Emmanuel Plougoulm <contact@emmanuelplougoulm.fr>
2017-10-31 14:55:36 +0100

    suppression doublons dossiers

--- public/js/ancres.js
+++
@@ -1,9 +0,0 @@
-
-
-//Ancre ateliers
-
-$(".sliding-link").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
-});
