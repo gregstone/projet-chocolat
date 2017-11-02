b80b88379c6e285354de8f73a7c5dd212c45ebc8
Emmanuel Plougoulm <contact@emmanuelplougoulm.fr>
2017-11-02 18:42:28 +0100

    creating new routes for atelier

--- routes/admin.js
+++ routes/admin.js
@@ -2,131 +2,155 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 const config = require('../config.js');
-const connection =  mysql.createConnection(config);
+const connection = mysql.createConnection(config);
 
 connection.connect();
 
-
 // GET /admin
-router.get('/', function(req, res){
-	// page de login (formulaire)
-	res.render('login');
+router.get('/', function(req, res) {
+    // page de login (formulaire)
+    res.render('login');
 });
 
 
 // POST /admin/login
-router.post('/', function(req, res){
+router.post('/', function(req, res) {
 
-	// page de login 
-	// puis
-	// redirection vers /admin/logged (page d'accueil de l'espace admin)
-	let login = req.body.login;
-	let password= req.body.password;
-	connection.query('SELECT * FROM admin  WHERE login = "' + login + '" AND password = "' + password + '";', function (error, results, fields) {
-	  if (error) throw error;
-	  
-	  if (results.length === 0) {
-	  	res.send("Cet utilisateur n'existe pas");
-	  } else {
-	  	req.session.connected = true;
-	  	res.redirect('/admin/logged');
-	  }
-	});
+    // page de login 
+    // puis
+    // redirection vers /admin/logged (page d'accueil de l'espace admin)
+    let login = req.body.login;
+    let password = req.body.password;
+    connection.query('SELECT * FROM admin  WHERE login = "' + login + '" AND password = "' + password + '";', function(error, results, fields) {
+        if (error) throw error;
+
+        if (results.length === 0) {
+            res.send("Cet utilisateur n'existe pas");
+        } else {
+            req.session.connected = true;
+            res.redirect('/admin/logged');
+        }
+    });
 });
 
 // GET admin/logged
-router.get('/logged', function(req, res){
-	
-	if (req.session.connected){
-	 	res.render('admin');
-	} else {
-	 	res.redirect('/');
-	}
+router.get('/logged', function(req, res) {
+
+    if (req.session.connected) {
+        res.render('admin');
+    } else {
+        res.redirect('/');
+    }
 });
 
 
 // GET /SESSION OUVERTE
-router.get('/login', function(req, res){
-	
+router.get('/login', function(req, res) {
+
 });
-
-
-
 
 
 // GET /admin/produits
-router.get('/produits', function(req, res){
-	// liste des produits
+router.get('/produits', function(req, res) {
+    // liste des produits
 });
 
 // GET /admin/produits/ajouter
-router.get('/produits/ajouter', function(req, res){
-	// ajouter des produits (formulaire)
+router.get('/produits/ajouter', function(req, res) {
+    // ajouter des produits (formulaire)
 });
 
 
 // POST /admin/produits/ajouter
-router.post('/produits/ajouter', function(req, res){
-	// ajouter des produits
-	// puis
-	// redirection vers /produits
+router.post('/produits/ajouter', function(req, res) {
+    // ajouter des produits
+    // puis
+    // redirection vers /produits
 });
 
 // GET /admin/produits/modifier
-router.get('/produits/modifier/:id(\\d+)', function(req, res){ // req.params.id 
-	// modifier des produits (formulaire)
+router.get('/produits/modifier/:id(\\d+)', function(req, res) { // req.params.id 
+    // modifier des produits (formulaire)
 });
 
 // POST /admin/produits/modifier
-router.post('/produits/modifier/:id(\\d+)', function(req, res){
-	// modifier des produits
-	// puis
-	// redirection vers /produits
+router.post('/produits/modifier/:id(\\d+)', function(req, res) {
+    // modifier des produits
+    // puis
+    // redirection vers /produits
 });
 
 // GET /admin/produits/supprimer
-router.get('/produits/supprimer/:id(\\d+)', function(req, res){
-	// supprimer des produits
-	// puis
-	// redirection vers /produits
+router.get('/produits/supprimer/:id(\\d+)', function(req, res) {
+    // supprimer des produits
+    // puis
+    // redirection vers /produits
 });
 
 
-// GET /admin/ateliers
-router.get('/ateliers', function(req, res){
-	// liste des ateliers
+// GET /admin/ateliers   // OK !!!!! Manque l'affichage de l'image dans la base ///
+router.get('/ateliers', function(req, res) {
+    connection.query('SELECT * FROM workshops', function(error, results, fields) {
+        if (error) throw error;
+        // connected!
+        //res.render('admin-ateliers', results);
+
+        res.render('admin-ateliers.pug', {
+            results: results
+        });
+        console.log(results);
+        // liste des ateliers
+    });
 });
 
-// GET /admin/ateliers/ajouter
-router.get('/ateliers/ajouter', function(req, res){
-	// ajouter des ateliers (formulaire)
+// GET /admin/ateliers/modifier // OK //
+router.get('/ateliers/modifier', function(req, res) {
+    res.render('admin-ateliers-modifier.pug', {});
+    //ajouter des ateliers (formulaire)
 });
 
 
-// POST /admin/ateliers/ajouter
-router.post('/ateliers/ajouter', function(req, res){
-	// ajouter des ateliers
-	// puis
-	// redirection vers /ateliers
+// POST /admin/ateliers/modifier
+
+// modifier l'atelier en cours (formulaire) ps: on ne peut que modifier puisqu'il n'y en a qu'un à la fois
+router.post('/ateliers/modifier', function(req, res) {
+
+    //if(req.file){} // si req.file existe
+    //if (req.file.size < (3*1024*1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpg') ) {
+    //fs.rename(req.file.path,'public/images/'+req.body.product_namereq.file.originalname);
+    //} else {
+    //res.send('Vous avez fait une erreur dans le téléchargement') }
+	console.log(req.body);
+    connection.query('INSERT INTO workshops VALUES (null, ?, ?,null )', [req.body.name, req.body.date], function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        // connected!
+        res.redirect('/admin/ateliers');
+
+
+    });
+
+    // puis
+    // redirection vers /ateliers
 });
 
 // GET /admin/ateliers/modifier
-router.get('/ateliers/modifier/:id(\\d+)', function(req, res){
-	// modifier des ateliers (formulaire)
+router.get('/ateliers/modifier/:id(\\d+)', function(req, res) {
+    // modifier des ateliers (formulaire)
 });
 
 // POST /admin/ateliers/modifier
-router.post('/ateliers/modifier/:id(\\d+)', function(req, res){
-	// modifier des ateliers
-	// puis
-	// redirection vers /ateliers
+router.post('/ateliers/modifier/:id(\\d+)', function(req, res) {
+    // modifier des ateliers
+    // puis
+    // redirection vers /ateliers
 });
 
 // GET /admin/ateliers/supprimer
-router.get('/ateliers/supprimer/:id(\\d+)', function(req, res){
-	// supprimer des ateliers
-	// puis
-	// redirection vers /ateliers
+router.get('/ateliers/supprimer/:id(\\d+)', function(req, res) {
+    // supprimer des ateliers
+    // puis
+    // redirection vers /ateliers
 });
 
 module.exports = router;
